80c5681572c0d0107b09e95644aeef02775e9a94
Robert <rartesona@uoc.edu>
2026-04-12 11:45:44 +0200

    app espaiflex 12-04-2026b

--- routes/api.php
+++ routes/api.php
@@ -60,15 +60,16 @@ Route::get('/reserves/dia/{dia}/sala/{sala}/reserva/{reserva}', [ReservaControll
 
 // Comentaris
 Route::post('/comentaris', [ComentariController::class, 'store']);
-// Llegir a l'entrar al calendari
+// Llegir a l'entrar al calendari i veure sales tothom
 Route::get('/getsales/edifici/{edifici}', [SalaController::class, 'salasEdifici']);    
+Route::get('/getsales/versala/{id_sala}', [SalaController::class, 'verSala']); 
 
 Route::middleware('auth:sanctum')->group(function () {
     // Update User
     Route::put('/users/{id}', [UserController::class, 'update']);
     // Llegir Sales
     
-    Route::get('/getsales/versala/{id_sala}', [SalaController::class, 'verSala']);    
+       
     Route::get('/getsales/vercomplements/{id_sala}', [SalaController::class, 'verComplements']);    
     // Esborro reserva
     Route::get('/reserves/delete_event/{id_event}', [ReservaController::class, 'deleteEvent']);           
